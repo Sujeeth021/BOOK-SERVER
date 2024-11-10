6a520c9daa5ae73197f8b1f035e134e2dee214bb
sujith017 <sujithb934@gmail.com>
2024-11-10 21:32:15 +0530

    changed to module import

--- server/routes/api/books.js
+++ server/routes/api/books.js
@@ -1,14 +1,11 @@
-const express = require('express');
+import express from 'express';
 const router = express.Router();
-const booksController = require('../../controllers/booksController');
+import * as booksController from '../../controllers/booksController.js'; 
 
 router.route('/').get(booksController.getAllBooks);
-
 router.route('/:id').delete(booksController.deleteBook);
-
 router.route('/addBook').post(booksController.createNewBook);
-
 router.route('/editBook/:id').put(booksController.updateBook);
 router.route('/editBook/:id').get(booksController.getBook);
 
-module.exports = router;
+export default router; 
